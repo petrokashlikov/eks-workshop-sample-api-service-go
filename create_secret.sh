1fa2e15d5a1c1e14c02938d741e5b2d16a176ef9
petrokashlikov <petr.kashlikov@gmail.com>
2020-05-27 19:46:40 -0400

    testing ecr secret

---
+++ create_secret.sh
@@ -0,0 +1,12 @@
+
+SECRET_NAME=$AWS_REGION-ecr-registry
+EMAIL=dummy.email@email.com
+TOKEN=`aws ecr get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
+echo "ENV variables setup done."
+kubectl create secret docker-registry $SECRET_NAME \
+--docker-server=https://$REPOSITORY_URI \
+--docker-username=AWS \
+--docker-password="${TOKEN}" \
+--docker-email="${EMAIL}"
+echo "Secret created by name. $SECRET_NAME"
+kubectl patch serviceaccount default -p '{"imagePullSecrets":[{"name":"'$SECRET_NAME'"}]}'
