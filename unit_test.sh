a31fd5fcc1170330f7ddfc0171ce10d7a5be0120
petrokashlikov <petr.kashlikov@gmail.com>
2020-05-27 21:14:04 -0400

    checking build stage failure

--- unit_test.sh
+++ unit_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-api_host=$(kubectl get svc hello-k8s -o json | jq -r .status.loadBalancer.ingress[].ip)
+api_host=$(kubectl get svc hello-k8s -o json | jq -r .status.loadBalancer.ingress[].i)
 curl -m 2 $api_host
 res=$?
 if test "$res" != "0"; then
