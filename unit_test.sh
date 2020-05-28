def1521b1676ef8d521743c7ff20960426d9472c
petrokashlikov <petr.kashlikov@gmail.com>
2020-05-27 21:08:11 -0400

    adding unit test

---
+++ unit_test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+api_host=$(kubectl get svc hello-k8s -o json | jq -r .status.loadBalancer.ingress[].ip)
+curl -m 2 $api_host
+res=$?
+if test "$res" != "0"; then
+   echo "the curl command failed with: $res"
+fi
+
