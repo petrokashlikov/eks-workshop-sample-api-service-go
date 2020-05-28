91cd0fda41240930fa56b792ff497cde2b388729
petrokashlikov <petr.kashlikov@gmail.com>
2020-05-27 21:38:55 -0400

    update error handling

--- unit_test.sh
+++ unit_test.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
+set -e
 api_host=$(kubectl get svc hello-k8s -o json | jq -r .status.loadBalancer.ingress[].i)
 curl -m 2 $api_host
-res=$?
-if test "$res" != "0"; then
-   echo "the curl command failed with: $res"
-fi
 
