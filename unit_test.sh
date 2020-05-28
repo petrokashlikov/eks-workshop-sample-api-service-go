88369fe9b71d96403c4d01802c2bbadde039d325
petrokashlikov <petr.kashlikov@gmail.com>
2020-05-27 21:45:12 -0400

    update unit test

--- unit_test.sh
+++ unit_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-api_host=$(kubectl get svc hello-k8s -o json | jq -r .status.loadBalancer.ingress[].i)
+api_host=$(kubectl get svc hello-k8s -o json | jq -r .status.loadBalancer.ingress[].ip)
 curl -m 2 $api_host
 
